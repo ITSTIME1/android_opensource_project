7aade358090666102969e8867dd5108300ae2321
ITSTIME <itstime0809@gmail.com>
2022-04-17 14:58:10 +0900

    Update : Material Design Practice :)

--- InstagramClone/app/src/main/java/com/example/instagramclone/MainActivity.java
+++ InstagramClone/app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivty";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Start Activity");
+
+
+
+    }
+    private void setupBottomNavigation() {
+        Log.d(TAG, "setupNavigation: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavigation);
+
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
     }
 }
