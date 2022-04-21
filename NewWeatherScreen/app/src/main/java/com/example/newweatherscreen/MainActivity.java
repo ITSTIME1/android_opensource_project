9f840dad6c0a5e670953e285e70080c0a4e57f5b
ITSTIME <itstime0809@gmail.com>
2022-04-21 23:55:01 +0900

    Update : weather screen add & button function add :)

--- NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/MainActivity.java
+++ NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.newweatherscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button move_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_home);
+        setContentView(R.layout.activity_main);
+
+        move_button = (Button) findViewById(R.id.move_button);
+
+        move_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/SecondActivity.java
+++ NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/SecondActivity.java
@@ -1,15 +1,34 @@
 package com.example.newweatherscreen;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Button seoul_button;
+    HorizontalScrollView horizontalScrollView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_home);
+
+
+        seoul_button = (Button) findViewById(R.id.seoul_button);
+        horizontalScrollView = (HorizontalScrollView) findViewById(R.id.scrollview);
+
+        seoul_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalScrollView.pageScroll(1);
+            }
+        });
     }
 }
