0362c8f710723438cfa4dbc360ddcc7ae60d67eb
ITSTIME <itstime0809@gmail.com>
2022-04-22 20:16:00 +0900

    Update : scroll function update :)

--- NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/SecondActivity.java
+++ NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.newweatherscreen;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +13,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
-    Button seoul_button;
+    Button tokyo_button;
+    Button second_button;
+    Button bamboo_button;
+    Button jeju_button;
     HorizontalScrollView horizontalScrollView;
     ImageView second_view;
     ImageView first_view;
+    ImageView three_view;
+    ImageView last_view;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,17 +29,47 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.layout_home);
 
 
-        seoul_button = (Button) findViewById(R.id.seoul_button);
+        tokyo_button = (Button) findViewById(R.id.tokyo_button);
+        second_button = (Button) findViewById(R.id.second_button);
+        bamboo_button = (Button) findViewById(R.id.bamboo_button);
+        jeju_button = (Button) findViewById(R.id.jeju_button);
         horizontalScrollView = (HorizontalScrollView) findViewById(R.id.scrollview);
-        second_view = (ImageView) findViewById(R.id.second_view);
-        first_view = (ImageView) findViewById(R.id.first_view);
 
-        seoul_button.setOnClickListener(new View.OnClickListener() {
+
+        first_view = (ImageView) findViewById(R.id.first_view);
+        second_view = (ImageView) findViewById(R.id.second_view);
+        three_view = (ImageView) findViewById(R.id.three_view);
+        last_view = (ImageView) findViewById(R.id.last_view);
+
+        tokyo_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // 전체 horizontalScrollView 의 Width 길이 - 원하고자 하는 Second_View의 width /2 로 나눈 값만큼 이동시킴
-                horizontalScrollView.scrollTo(horizontalScrollView.getWidth()  - second_view.getWidth() / 2, 0);
+                horizontalScrollView.scrollTo(0, 0);
             }
         });
+
+        second_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalScrollView.scrollTo(horizontalScrollView.getWidth() - second_view.getWidth()/2, 0);
+            }
+        });
+
+        bamboo_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalScrollView.scrollTo(horizontalScrollView.getWidth() - (three_view.getWidth()/2 - last_view.getWidth()), 0);
+            }
+        });
+
+        jeju_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                horizontalScrollView.scrollTo(last_view.getRight(), 0);
+            }
+        });
+
     }
+
 }
