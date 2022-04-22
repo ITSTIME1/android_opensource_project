41cdd8f0e5ec01acc23fe878c51d3da6fc8bea61
ITSTIME <itstime0809@gmail.com>
2022-04-22 18:35:38 +0900

    Update : ScrollTo Function Implement :)

--- NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/SecondActivity.java
+++ NewWeatherScreen/app/src/main/java/com/example/newweatherscreen/SecondActivity.java
@@ -14,6 +14,8 @@ public class SecondActivity extends AppCompatActivity {
 
     Button seoul_button;
     HorizontalScrollView horizontalScrollView;
+    ImageView second_view;
+    ImageView first_view;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,11 +25,14 @@ public class SecondActivity extends AppCompatActivity {
 
         seoul_button = (Button) findViewById(R.id.seoul_button);
         horizontalScrollView = (HorizontalScrollView) findViewById(R.id.scrollview);
+        second_view = (ImageView) findViewById(R.id.second_view);
+        first_view = (ImageView) findViewById(R.id.first_view);
 
         seoul_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                horizontalScrollView.pageScroll(1);
+                // 전체 horizontalScrollView 의 Width 길이 - 원하고자 하는 Second_View의 width /2 로 나눈 값만큼 이동시킴
+                horizontalScrollView.scrollTo(horizontalScrollView.getWidth()  - second_view.getWidth() / 2, 0);
             }
         });
     }
