7aa2429e60a53a44d437feae6e507ba27f9c0c73
ITSTIME <itstime0809@gmail.com>
2022-04-23 10:51:21 +0900

    Update : SignIn Intent & Login Design Complete :)

---
+++ Food/app/src/main/java/com/example/food/RegisterActivity.java
@@ -0,0 +1,45 @@
+package com.example.food;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Button register_complete;
+    TextView signIn_page;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_view);
+
+
+        register_complete = (Button) findViewById(R.id.Register_complete_button);
+        signIn_page = (TextView) findViewById(R.id.SignIn_page_button);
+
+
+        register_complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Main Home Page Section
+            }
+        });
+
+        signIn_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn_page_intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(signIn_page_intent);
+                finish();
+            }
+        });
+
+    }
+}
