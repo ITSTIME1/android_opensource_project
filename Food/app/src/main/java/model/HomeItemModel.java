ad365302235544da7348a8f82c7ce8c4efa775c5
ITSTIME <itstime0809@gmail.com>
2022-04-25 02:56:21 +0900

    Update : RecyclerView Annotation add :)

---
+++ Food/app/src/main/java/model/HomeItemModel.java
@@ -0,0 +1,35 @@
+package model;
+
+
+// * HomeItem Model
+
+
+public class HomeItemModel {
+
+    int image;
+    String name;
+
+    // HomeItem Constructor
+    HomeItemModel(int image, String name) {
+        this.image = image;
+        this.name = name;
+    }
+
+    // Import Image Method
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    // Import Name Method
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
