24b4f4a10b4337df87f9306f7826123ff0fd3157
ITSTIME <itstime0809@gmail.com>
2022-04-28 21:49:27 +0900

    Update : login, register Design :)

--- FoodRestuarant/app/src/main/java/com/example/foodrestuarant/RegisterActivity.java
+++ FoodRestuarant/app/src/main/java/com/example/foodrestuarant/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.foodrestuarant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
--- FoodRestuarant/app/src/main/java/com/example/foodrestuarant/WelcomeActivity.java
+++ FoodRestuarant/app/src/main/java/com/example/foodrestuarant/WelcomeActivity.java
@@ -1,6 +1,8 @@
 package com.example.foodrestuarant;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,10 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(WelcomeActivity.this, RegisterActivity.class));
     }
 }
