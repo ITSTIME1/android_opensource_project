ab0f0d8c6e0ee931d0ca722ab50c0db0f87faf43
ITSTIME <itstime0809@gmail.com>
2022-05-11 11:40:56 +0900

    Update : searchView change & keyboard funtion :)

--- Ecommerce/app/src/main/java/Fragments/SearchFragment.java
+++ Ecommerce/app/src/main/java/Fragments/SearchFragment.java
@@ -1,32 +1,69 @@
 package Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
 import com.example.ecommerce.R;
 
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment{
 
     private SearchView searchView;
-    private String searchViewMessage = "Search..";
+    private ConstraintLayout constraintLayout;
+    private NestedScrollView nestedScrollView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View searchRoot = inflater.inflate(R.layout.fragment_search, container, false);
 
         searchView = searchRoot.findViewById(R.id.searchView);
-        searchView.setQuery(searchViewMessage, false);
-        searchView.clearFocus();
+        constraintLayout = searchRoot.findViewById(R.id.search_constraint_layout);
+        nestedScrollView = searchRoot.findViewById(R.id.search_nested_scrollView);
+
+
+        constraintLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                hideKeyboard();
+                return false;
+            }
+        });
+
+        nestedScrollView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                hideKeyboard();
+                return false;
+            }
+        });
 
         return searchRoot;
     }
+
+
+    // ** Keyboard Hide **
+
+    private void hideKeyboard()
+    {
+        if (getActivity() != null && getActivity().getCurrentFocus() != null)
+        {
+            // 프래그먼트기 때문에 getActivity() 사용
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+
+
 }
