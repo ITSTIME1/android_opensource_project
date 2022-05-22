8b8e00d7c18785c9745691e4f28e59575b8b1f44
ITSTIME <itstime0809@gmail.com>
2022-05-22 23:52:58 +0900

    Problem : Android 12 SplashScreen not showing icon :(

--- GitLove/app/src/main/java/com/example/gitlove/MainActivity.java
+++ GitLove/app/src/main/java/com/example/gitlove/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.gitlove;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +8,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
