0996b4b73d80e2b9a4578233e5aa43139377e5ec
ITSTIME <itstime0809@gmail.com>
2022-05-24 02:44:19 +0900

    Update : StatusBar Change Method add :)

--- GitLove/app/src/main/java/com/example/gitlove/MainActivity.java
+++ GitLove/app/src/main/java/com/example/gitlove/MainActivity.java
@@ -2,31 +2,60 @@ package com.example.gitlove;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import Interface.StatusBarBuildVersionCheck;
 
 
-public class MainActivity extends AppCompatActivity implements StatusBarBuildVersionCheck {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         com.example.gitlove.databinding.ActivityMainBinding activityMainBinding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
-        statusBarBuildVersionCheck();
+
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
     }
 
-    @SuppressLint("ObsoleteSdkInt")
-    @Override
-    public void statusBarBuildVersionCheck() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    /**
+     * [StatusBar && BottomNavigation Both transparent true]
+     */
+//    @SuppressLint("ObsoleteSdkInt")
+//    @Override
+//    public void statusBarBuildVersionCheck() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            Window w = getWindow();
+//            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+//                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//        }
+//    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
         }
+        win.setAttributes(winParams);
     }
 }
