bff3b10032bc36e2d254e4d73cf5bfce049ae388
ITSTIME <itstime0809@gmail.com>
2022-06-10 23:34:58 +0900

    Update : RegisterViewModel & RegisterActivity bug fix & Authentication userUID bug fix :)

--- firebase_chat_basic/app/src/main/java/com/example/firebase_chat_basic/view/activity/RegisterActivity.java
+++ firebase_chat_basic/app/src/main/java/com/example/firebase_chat_basic/view/activity/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.firebase_chat_basic.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -53,28 +55,19 @@ public class RegisterActivity extends AppCompatActivity implements BaseInterface
         BaseInterface.super.observerIntent();
         registerViewModel.getDataList.observe(this, registerData -> {
 
-            // Activity -> Activity
-            // 이름, 이메일, uid, image 전송.
             Intent registerIntent = new Intent(this, MainActivity.class);
-            registerIntent.putExtra("clientName", registerData.get(0));
-            registerIntent.putExtra("clientEmail", registerData.get(1));
-            registerIntent.putExtra("clientUUID", registerData.get(3));
-            registerIntent.putExtra("clientProfileImage", registerData.get(4));
 
-            if(registerData.get(0) != null && registerData.get(1) != null && registerData.get(3) != null && registerData.get(4) != null) {
-                startActivity(registerIntent);
-                finish();
+            if(registerData.get(0) != null && registerData.get(1) != null && registerData.get(2) != null) {
+                registerIntent.putExtra("clientUID", registerData.get(0));
+                registerIntent.putExtra("clientName", registerData.get(1));
+                registerIntent.putExtra("clientEmail", registerData.get(2));
+                registerIntent.putExtra("clientProfileImage", registerData.get(3));
+                Log.d("registerIntent", "success");
             } else {
-                System.out.println("registerData 값이 null이 있습니다.");
+                Log.d("registerData", "register Data null");
             }
-
-            System.out.println("=============================");
-            System.out.println("RegisterActivity - succeeded");
-            System.out.println(registerData.get(0).toString());
-            System.out.println(registerData.get(1).toString());
-            System.out.println(registerData.get(3).toString());
-            System.out.println(registerData.get(4).toString());
-            System.out.println("=============================");
+            startActivity(registerIntent);
+            finish();
         });
     }
 }
--- firebase_chat_basic/app/src/main/java/com/example/firebase_chat_basic/viewModel/RegisterViewModel.java
+++ firebase_chat_basic/app/src/main/java/com/example/firebase_chat_basic/viewModel/RegisterViewModel.java
@@ -1,4 +1,6 @@
 package com.example.firebase_chat_basic.viewModel;
+import android.os.Handler;
+import android.util.Log;
 import android.util.Patterns;
 import android.widget.Toast;
 
@@ -52,13 +54,14 @@ public class RegisterViewModel extends ViewModel{
     private FirebaseAuth firebaseAuth;
 
     // two-way dataBinding
-    public MutableLiveData<String> getRegisterFirstName = new MutableLiveData<>();
-    public MutableLiveData<String> getRegisterSecondName = new MutableLiveData<>();
-    public MutableLiveData<String> getRegisterEmail = new MutableLiveData<>();
-    public MutableLiveData<String> getRegisterPassword = new MutableLiveData<>();
-    public MutableLiveData<String> getProfileImage = new MutableLiveData<>();
+    public MutableLiveData<String> getRegisterFirstName;
+    public MutableLiveData<String> getRegisterSecondName;
+    public MutableLiveData<String> getRegisterEmail;
+    public MutableLiveData<String> getRegisterPassword;
+    public MutableLiveData<String> getProfileImage;
     // mutableLiveData list
     public MutableLiveData<ArrayList<String>> getDataList = new MutableLiveData<>();
+
 
 
     // firebase uuid, emailPattern, passwordPattern
@@ -68,11 +71,25 @@ public class RegisterViewModel extends ViewModel{
     public int passwordPattern = 6;
 
 
+    ArrayList<String> stringArrayList;
+    String checkFirstName;
+    String checkSecondName;
+    String checkEmail;
+    String checkPassword;
+    String checkProfileImage;
+    String checkName;
+
     // registerViewModel constructor
     public RegisterViewModel() {
         databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl(realTimeDataBaseUserUrl);
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        stringArrayList = new ArrayList<>();
+        getRegisterFirstName = new MutableLiveData<>();
+        getRegisterSecondName = new MutableLiveData<>();
+        getRegisterEmail = new MutableLiveData<>();
+        getRegisterPassword = new MutableLiveData<>();
+        getProfileImage = new MutableLiveData<>();
 
     }
 
@@ -83,8 +100,45 @@ public class RegisterViewModel extends ViewModel{
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
-                    getFirebaseUserUID = firebaseUser.getUid();
-                    System.out.print("파이어베이스 UID" + getFirebaseUserUID);
+                    System.out.println("성공");
+
+                    final String currentUserUID = firebaseUser.getUid();
+                    // temp variable
+                    String finalCheckProfileImage = checkProfileImage;
+
+
+                    stringArrayList.add(currentUserUID);
+                    stringArrayList.add(checkName);
+                    stringArrayList.add(checkEmail);
+                    stringArrayList.add(finalCheckProfileImage);
+
+                    getDataList.setValue(stringArrayList);
+                    Log.d("getDataList", "into data success");
+
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.child("users").hasChild(currentUserUID)){
+                                System.out.println("이미 이 계정은 존재해요");
+                            } else {
+
+                                // 1. 리얼타임데이터베이스에 저장 email, name
+                                databaseReference.child("users").child(currentUserUID).child("uid").setValue(currentUserUID);
+                                databaseReference.child("users").child(currentUserUID).child("name").setValue(checkName);
+                                databaseReference.child("users").child(currentUserUID).child("email").setValue(checkEmail);
+                                // 2. 프로필 이미지는 처음엔 기본 아무것도 없는 상태로 저장.
+                                databaseReference.child("users").child(currentUserUID).child("profileImage").setValue(finalCheckProfileImage);
+                                Log.d("realtimeDataBase", "into realtimeDataBase success");
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            System.out.print(error);
+                        }
+                    });
+
+
                 } else {
                     System.out.println("값이 성공적으로 저장이 되지 않았어요");
                 }
@@ -96,78 +150,22 @@ public class RegisterViewModel extends ViewModel{
 
     // firebase realTimebase add data
     public void registerButton(){
-
-        // realtime database 저장용.
-        ArrayList<String> stringArrayList = new ArrayList<>();
         // firebase password 길이 확인용.
         ArrayList<String> validationPassword = new ArrayList<>();
 
         // get data from EditText
-        String checkFirstName = getRegisterFirstName.getValue();
-        String checkSecondName = getRegisterSecondName.getValue();
-        String checkEmail = getRegisterEmail.getValue();
-        String checkPassword = getRegisterPassword.getValue();
-        String checkProfileImage = getProfileImage.getValue();
-        String checkName = checkFirstName + checkSecondName;
+        checkFirstName = getRegisterFirstName.getValue();
+        checkSecondName = getRegisterSecondName.getValue();
+        checkEmail = getRegisterEmail.getValue();
+        checkPassword = getRegisterPassword.getValue();
+        checkProfileImage = getProfileImage.getValue();
+        checkName = checkFirstName + checkSecondName;
+
         if(checkProfileImage == null) checkProfileImage = "Default";
-
-
         validationPassword.add(checkPassword);
 
-        assert checkEmail != null;
-        // 1. 이메일 조건에도 부합하고, 패스워드 길이도 부합한다.
-        // 2. 이메일, 패스워드가 비어있지 않다.
-        // 3. 가입이 완료된다면 배열에 값들을 전부 저장시켜주고
+        // firebase createUser
         firebaseRegister(checkEmail, checkPassword);
-        if(emailPattern.matcher(checkEmail).matches() && validationPassword.size() > passwordPattern && checkPassword != null) {
-            System.out.print("파이어베이스 name : " + checkName);
-            System.out.print("파이어베이스 email : " + checkEmail);
-            System.out.print("파이어베이스 password : " + checkPassword);
-            // firebaseCurrentUserID
-            System.out.println("파이어베이스 UUID : " + getFirebaseUserUID);
-
-
-            // into stringArrayList
-            stringArrayList.add(checkName);
-            stringArrayList.add(checkEmail);
-            stringArrayList.add(checkPassword);
-            stringArrayList.add(getFirebaseUserUID);
-            stringArrayList.add(checkProfileImage);
-
-            if(checkProfileImage == null) {
-                checkProfileImage = "Default";
-            }
-
-            // stringArrayList into getDataList
-            getDataList.setValue(stringArrayList);
-
-
-            // temp variable
-            String finalCheckProfileImage = checkProfileImage;
-            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.child("users").hasChild(checkName)){
-                        System.out.println("이미 이 계정은 존재해요");
-                    } else {
-
-                        // 1. 리얼타임데이터베이스에 저장 email, name
-                        databaseReference.child("users").child(getFirebaseUserUID).child("email").setValue(checkEmail);
-                        databaseReference.child("users").child(getFirebaseUserUID).child("name").setValue(checkName);
-                        // 2. 프로필 이미지는 처음엔 기본 아무것도 없는 상태로 저장.
-                        databaseReference.child("users").child(getFirebaseUserUID).child("profileImage").setValue(finalCheckProfileImage);
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    System.out.print(error);
-                }
-            });
-
-        } else {
-            System.out.println("이메일 이나 패스워드를 다시 확인이 필요합니다.");
-        }
-
     }
+
 }
